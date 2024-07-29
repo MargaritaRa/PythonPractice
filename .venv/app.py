ef3f2b24cf073a82c6b354133d5d2df84089ad70
Margarita Ramirez <margarita26.mr@gmail.com>
2024-07-29 16:26:48 -0400

    routes

--- .venv/app.py
+++ .venv/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 # how to create the application:
 app = Flask(__name__)
 
@@ -6,6 +6,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Hello World</h1>"
+
+
+@app.route('/hello')
+def hello():
+      response = make_response("hello world")
+      response.status_code = 202
+      response.headers['content-type'] = "text/plain"
+      return response
+
+@app.route('/greet/<name>')
+def greet(name):
+     return f"Hello {name}"
+# dinamic url, url processers
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+     return f'{number1} + {number2} = {number1 + number2}'
+
+@app.route('/handle_url_params')
+def handle_params():
+     if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args['greeting']
+        name = request.args.get('name')
+        return f'{greeting}, {name}'
+     else:
+          return "some parameters are missing"
+
 
 
 if __name__ == '__main__':
