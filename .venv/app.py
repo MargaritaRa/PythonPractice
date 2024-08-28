4751ce2f242a4051108a79bfe6dc7cdd3ec10db8
Margarita Ramirez <margarita26.mr@gmail.com>
2024-08-28 15:01:25 -0400

    completed file send post request

--- .venv/app.py
+++ .venv/app.py
@@ -2,7 +2,7 @@ import os
 # for unique ids
 import uuid
 import pandas as pd
-from flask import Flask, render_template, request, Response, send_from_directory
+from flask import Flask, render_template, request, Response, send_from_directory, jsonify
 app = Flask(__name__, template_folder='templates' )
 
 
@@ -73,7 +73,15 @@ def convert_csv_two():
 def download(filename):
     return send_from_directory('downloads', filename, as_attachment=True, download_name='result.csv')
 
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    greeting = request.json['greeting']
+    name = request.json['name']
 
+    with open('file.txt', 'w') as f:
+        f.write(f'{greeting}, {name}')
+
+    return jsonify({"message": "Successfully written!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
